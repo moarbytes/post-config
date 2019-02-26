cf734e79b39c2b77ace3a356be2de60360a39598
moarbytes <moarbytes@gmail.com>
2019-02-26 09:15:16 -0600

    cleaning
    Only ever runs on dedicated server, don't need to check for it..

--- scripts/ovh/ovh.sh
+++ scripts/ovh/ovh.sh
@@ -13,38 +13,13 @@ EXPECT_SCRIPT_PATH="/tmp/script"
 
 # Remote scripts URI root
 URI_ROOT="https://raw.githubusercontent.com/"\
-"MatthieuMichon/post-config/master/"
+"moarbytes/post-config/master/"
 
 # OS Release file
 OSR_FILE="/etc/os-release"
 
 # Secondary script file
 NEXT_FILE=/tmp/next
-
-# Error codes
-ERR_OK=0
-ERR_NOT_DEDICATED=1
-ERR_OSR_FILE=2
-ERR_DISTRO=3
-ERR_TRAVIS=255
-
-
-check_ovh_dedicated () {
-  if ! [[ "${0}" == $EXPECT_SCRIPT_PATH ]]; then
-    err "unexpected script path ${0}: Not a dedicated server."
-    exit $ERR_NOT_DEDICATED
-  fi
-}
-
-
-get_distro () {
-  if ! [[ -r $OSR_FILE ]]; then
-    err "cannot access $OSR_FILE: No such file."
-    exit $ERR_OSR_FILE
-  fi
-  DISTRO=$(sed -n '/^ID=/s///p' /etc/os-release)
-}
-
 
 exec_remote () {
   # 'raw.githubusercontent.com' domain may not clear HTTPS CA checks
@@ -57,48 +32,6 @@ cfg_debian () {
   exec_remote "scripts/ovh/ovh_debian"
   exec_remote "scripts/debian/deb_host"
   exec_remote "scripts/debian/deb_lxc"
-}
-
-
-run_tests () {
-  err "test error message"
-  exec_remote "tests/returns_zero"
-  local retval=$?
-  if ! [[ $retval -eq 0 ]]; then
-    exit $ERR_TRAVIS
-  fi
-  exec_remote "tests/returns_one"
-  local retval=$?
-  if ! [[ $retval -eq 1 ]]; then
-    err "Returned $? instead of 1."
-    exit $ERR_TRAVIS
-  fi
-  exit 0
-}
-
-
-main () {
-  get_distro
-
-  if [[ $TRAVIS ]]; then
-    run_tests
-    exit $ERR_OK
-  fi
-
-  check_ovh_dedicated
-
-  case "$DISTRO" in
-    debian)
-      cfg_debian
-      ;;
-
-    *)
-      err "unsupported distribution: $DISTRO"
-      exit $ERR_DISTRO
-      ;;
-  esac
-  touch postconfig_completed
-  exit 1
 }
 
 
